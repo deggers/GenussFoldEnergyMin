1170d70d894fbe68a1e98a532cddf83cedee52f3
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2021-06-16 09:53:38 +0200

    Fix RG grammar - no nil case in Split-NonTerminal

--- BioInf/GenussFold/Grammars/RG.hs
+++ BioInf/GenussFold/Grammars/RG.hs
@@ -21,14 +21,17 @@ S: S      -- :: Startsymbol
 S -> nil <<< e
 S -> unp <<< c S
 S -> jux <<< c S c S
-S -> pkn <<< X Y X Y
+S -> pkn <<< X Y X Y   -- X S ::  Y S ::  X S :: Y S
 
--- TODO do I need for the only-closing case?
-<X,X> -> pk1 <<< [-,c] [-,S] <X,X> [c,-] [S,-]
-<X,X> -> nll <<< [e,e]
+-- z    z z z
+-- - X1 [ - S1  X[S
+-- ] X2 - S2 -  ]XS
 
-<Y,Y> -> pk2 <<< [-,c] [-,S] <Y,Y> [c,-] [S,-]
-<Y,Y> -> nll <<< [e,e]
+<X,X> -> pk1 <<< [-,c] <X,X> [c,-] [-,S] [S,-]
+<X,X> -> pk1b <<< [c,-] [-,c]
+
+<Y,Y> -> pk2 <<< [-,c] <Y,Y> [c,-] [-,S] [S,-]
+<Y,Y> -> pk2b <<< [c,-] [-,c]
 //
 Emit: PKN
 |]
